4b6c1e10e90f9e1a17fd442310129652057b1aea
Jorge <jgragia@epsg.upv.es>
2022-12-01 11:27:43 +0100

    Escena menu sin texturas

---
+++ Assets/Scripts/ComportamientoObjeto.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComportamientoObjeto : MonoBehaviour
+{
+    public void EmpezarJuego()
+    {
+
+    }
+
+    public void EmpezarVideo()
+    {
+
+    }
+
+    public void Controles()
+    {
+
+    }
+
+    public void Salir()
+    {
+
+    }
+
+}
