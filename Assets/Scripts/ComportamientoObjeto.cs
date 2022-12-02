fdf6cf0c84fc574f8c036deadaf8d6a1785efb7e
Jorge <jgragia@epsg.upv.es>
2022-12-02 13:08:52 +0100

    Progreso escena Menu

--- Assets/Scripts/ComportamientoObjeto.cs
+++ Assets/Scripts/ComportamientoObjeto.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Video;
 public class ComportamientoObjeto : MonoBehaviour
 {
+    private bool videoBool = false;
     public void EmpezarJuego()
     {
 
@@ -22,6 +23,17 @@ public class ComportamientoObjeto : MonoBehaviour
     public void Salir()
     {
 
+    }
+
+    public void PonerVideoEnTele()
+    {
+        if (!videoBool)
+        {
+            GameObject television = GameObject.FindGameObjectWithTag("video");
+            VideoPlayer video = television.GetComponent<VideoPlayer>();
+            video.Play();
+            videoBool = true;
+        }
     }
 
 }
