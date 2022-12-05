6c5c975d97db92b4629cf9cb0d824ba54ce93231
Jorge <jgragia@epsg.upv.es>
2022-12-05 15:47:00 +0100

    Logcat

--- Assets/MenuJugador.cs
+++ Assets/MenuJugador.cs
@@ -7,26 +7,42 @@ public class MenuJugador : MonoBehaviour
     [SerializeField]
     GameObject menu;
     bool menuBool = false;
+    bool esperar = false;
 
     public void Update()
     {
+        
         if (Input.GetMouseButtonDown(2))
         {
-            menuBool = !menuBool;
-            menu.SetActive(menuBool);
+            if (esperar == false)
+            {
+                StartCoroutine(Activar());
+            }
         }
-
+        
         if (Input.GetButton("A"))
         {
-            menuBool = !menuBool;
-            menu.SetActive(menuBool);
+            if(esperar == false){
+                StartCoroutine(Activar());
+            }   
+            
         }
 
-
+        /*
         if (Input.GetButton("AM"))
         {
             menuBool = !menuBool;
             menu.SetActive(menuBool);
         }
+        */
+    }
+
+    IEnumerator Activar()
+    {
+        menuBool = !menuBool;
+        esperar = true;
+        menu.SetActive(menuBool);
+        yield return new WaitForSeconds(.3f);
+        esperar = false;
     }
 }
